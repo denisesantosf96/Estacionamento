6989e00432eadb19b42097c0bd5ac5275e295135
Denise Ferreira <denise.santosf@hotmail.com>
2021-08-19 13:32:17 -0300

    Décimo oitavo commit

---
+++ Models/Veiculo.cs
@@ -0,0 +1,12 @@
+namespace Estacionamento.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public int IdTipoVeiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+    }
+}
